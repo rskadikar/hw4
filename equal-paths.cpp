80d72e59cd3e529bf548025bfd1175345087edae
Rachana Kadikar <kadikar@usc.edu>
2025-03-19 15:58:43 -0400

    equalPaths done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,27 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int getHeight(Node * root) {
+    if (root == nullptr) return 0;
+    if (getHeight(root -> left) > getHeight(root -> right)) {
+        return (1 + getHeight(root -> left));
+    }
+    else {
+        return (1 + getHeight(root -> right));
+    }
+}
+
+bool isLeaf(Node * root) {
+    if (root -> left == nullptr && root -> right == nullptr) return true;
+    else return false;
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    if (root == nullptr || isLeaf(root)) return true;
+    if (root -> left == nullptr) return equalPaths(root -> right);
+    if (root -> right == nullptr) return equalPaths(root -> left);
+    return (equalPaths(root -> left) && equalPaths(root -> right) && (getHeight(root -> left) == getHeight(root -> right)));
 }
 
--- equal-paths.h
+++ equal-paths.h
@@ -28,5 +28,7 @@ struct Node {
  * @param root Pointer to the root of the tree to check for equal paths
  */
 bool equalPaths(Node * root);
+int getHeight(Node * root);
+bool isLeaf(Node * root);
 
 #endif
